b2e8ee5981522344e4257e9efa6e2cbff17be2d2
PositiveTalkSpace <182389264+TheGenuineLoveProject@users.noreply.github.com>
2025-12-21 05:41:54 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,16 +1,6 @@
-{ pkgs }:
-
-{
+{ pkgs }: {
   deps = [
-    pkgs.gnutar
-    pkgs.zip
-    pkgs.u-root-cmds
-    pkgs.splat
-    pkgs.yakut
-    pkgs.imagemagick_light
-    pkgs.killall
-    pkgs.nodejs
-    pkgs.postgresql
-    pkgs.openssl
+    pkgs.nodejs_20
+    pkgs.git
   ];
 }
