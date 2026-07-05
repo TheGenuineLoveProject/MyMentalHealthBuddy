6490d8131666ada7967ee1bad016aad5f5c25103
Maria Landa <you@example.com>
2026-07-05 11:00:43 +0000

    E8D: include approval governance tests in account data runner

--- server/account/__tests__/runAccountDataTests.mjs
+++ server/account/__tests__/runAccountDataTests.mjs
@@ -3,6 +3,8 @@ import "./userDataEngines.test.mjs";
 import "./userDataExportContract.test.mjs";
 import "./userDataExportSectionMap.test.mjs";
 import "./userDataLifecyclePolicy.test.mjs";
+import "./userDataApprovalManifestHelpers.test.mjs";
+import "./userDataApprovalManifest.test.mjs";
 
 console.log({
   ok: true,
@@ -13,5 +15,7 @@ console.log({
     "userDataExportContract.test.mjs",
     "userDataExportSectionMap.test.mjs",
     "userDataLifecyclePolicy.test.mjs",
+    "userDataApprovalManifestHelpers.test.mjs",
+    "userDataApprovalManifest.test.mjs",
   ],
 });
